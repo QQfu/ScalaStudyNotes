f19a431c6e28c768c96da5b5a7b63319c6ec4c2e
Qing Quan, Fu <fuqingquandl@163.com>
2018-11-04 20:10:43 +0800

    Learning Scala - C5 - Implicits package notes - class ImplicitNotes - basic example

--- src/main/scala/notes/ImplicitNotes.scala
+++ src/main/scala/notes/ImplicitNotes.scala
@@ -2,6 +2,46 @@ package notes
 
 class ImplicitNotes {
   def basicExample():Unit = {
-    println("basic knowledge.")
+    //Scala可以有隐式定义，最简单的是隐式参数。
+
+    //multiTheNumber定义了一个隐式参数secNum，返回两数相乘的结果
+    def multiTheNumber(firNum: Double)(implicit secNum: Double) = {
+      firNum * secNum
+    }
+
+    //DoubleTheNumber类引用了multiTheNumber方法，并给隐式参数secNum赋值为2。
+    class DoubleTheNumber {
+      implicit val sec: Double = 2.0
+
+      def printResult(num: Double): Unit = {
+        println(multiTheNumber(num))
+      }
+    }
+
+    //结果为100
+    new DoubleTheNumber().printResult(50)
+    //结果为200
+    println(multiTheNumber(50)(4))
+
+    //隐式定义具有定域性。
+    object Imp1 {
+      implicit def secNum: Double = 3.0
+    }
+
+    object Imp2 {
+      implicit def secNum: Double = 5.0
+    }
+
+    {
+      //输出150
+      import Imp1.secNum
+      println(multiTheNumber(50))
+    }
+
+    {
+      //输出250
+      import Imp2.secNum
+      println(multiTheNumber(50))
+    }
   }
 }
